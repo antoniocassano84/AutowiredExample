b55b68128688454bbad4f62fe73c7f4733a639c8
antoniocassano84 <antonio.cassano984@gmail.com>
2020-06-11 05:59:46 +0100

    log statements added

--- src/main/java/com/apress/prospring5/ch3/StandardOutMessageRenderer.java
+++ src/main/java/com/apress/prospring5/ch3/StandardOutMessageRenderer.java
@@ -10,6 +10,7 @@ public class StandardOutMessageRenderer implements MessageRenderer {
 
   @Override
   public void render() {
+      System.out.println("-> calling render() in StandardOutMessageRenderer class");
       if(messageProvider == null) {
         throw new RuntimeException("You must set the provider for " +
                 StandardOutMessageRenderer.class.getName());
@@ -20,6 +21,8 @@ public class StandardOutMessageRenderer implements MessageRenderer {
   @Override
   @Autowired
   public void setMessageProvider(MessageProvider provider) {
+      System.out.println("-> calling setMessageProvider in StandardOutMessageRenderer");
+      System.out.println("-> injecting: " + provider.getClass().getName());
       this.messageProvider = provider;
   }
 
